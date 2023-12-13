58b016283179dc840e1c32e49cd5055b799f351c
nutan-mali <nutansm123@gmail.com>
2023-12-13 16:16:12 +0530

    Update admin.py

--- app1/admin.py
+++ app1/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from .models import Vendor,PurchaseOrder,PerformanceRecord
-<<<<<<< HEAD
-=======
-
->>>>>>> 402b54792de28a67ab904c281c3e13062fb183ea
-
 
 # Register your models here.
 admin.site.register(Vendor)
 admin.site.register(PurchaseOrder)
-<<<<<<< HEAD
+
 admin.site.register(PerformanceRecord)
-=======
+
 admin.site.register(PerformanceRecord)
->>>>>>> 402b54792de28a67ab904c281c3e13062fb183ea
+
